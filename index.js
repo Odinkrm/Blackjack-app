0edc960f48c64f4b3084fede1535fce9d1ae3759
Odin <asal.karimi.b@gmail.com>
2022-07-07 12:36:27 +0430

    Update generateRandomNum() to match Blackjack rules

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ let cards = [];
 let sum = 0;
 
 function generateRandomNum() {
-  return Math.floor(Math.random() * 13 + 1);
+  let randomNum = Math.floor(Math.random() * 13 + 1);
+  if (randomNum > 10) {
+    return 10;
+  } else if (randomNum === 1) {
+    return 11;
+  } else {
+    return randomNum;
+  }
 }
 function updateSum() {
   for (let i = 0; i < cards.length; i++) {
